c1afe92c3abf4dc7e48bdccb7a5dc0d9e77264e4
eoghan523 <eoghan523@gmail.com>
2024-11-16 00:21:25 +0000

    So far not broken, Ive created a urls.py and added code. I also pasted the correct url code for the orders_app folder

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
